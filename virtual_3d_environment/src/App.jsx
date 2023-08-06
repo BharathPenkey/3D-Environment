3b76a75c3e9e90a13f4a3e374e3fb88433381e04
BharathPenkey <penkybharath@gmail.com>
2023-08-06 09:44:09 +0530

    created animations and audio to the avatar and added functionalities

--- virtual_3d_environment/src/App.jsx
+++ virtual_3d_environment/src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 5]}}>
+    <Canvas shadows camera={{ position: [0, 1, 6]}}>
       <Experience />
     </Canvas>
   );
